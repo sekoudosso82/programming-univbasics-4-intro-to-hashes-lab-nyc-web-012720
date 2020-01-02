20054399e4dcc765a23a94dd1af2adbfe88ce79e
Sekou Dosso <sekou.dosso82@gmail.com>
2020-01-02 16:50:40 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,6 +22,7 @@ def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
   newHash = {} 
   newHash[key] = value 
+  newHash 
 end
 
 def read_from_hash(hash, key)
